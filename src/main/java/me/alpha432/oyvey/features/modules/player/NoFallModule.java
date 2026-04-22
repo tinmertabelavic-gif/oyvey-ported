f375f6972edc5c75b042d6faab8750a6beeb94b9
tinmertabelavic-gif <tinmertabelavic@gmail.com>
2026-04-22 17:13:52 +0200

    Update NoFallModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
@@ -6,7 +6,7 @@ import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 
 public class NoFallModule extends Module {
     public NoFallModule() {
-        super("NoFall", "Removes fall damage", Category.PLAYER);
+        super("AnkleInsurance", "Removes fall damage", Category.PLAYER);
     }
 
     @Override
